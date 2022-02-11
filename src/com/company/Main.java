c09ecd66aaff7a07a5b86290af9bd9277d643874
Katharina <katharina.roth-tro@gmx.de>
2022-02-11 08:48:55 +0100

    Aktueller

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Das ist mein GitHub Test 2022");
+    }
+}
---
+++ src/com/company/Zufall.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public class Zufall {
+    private int x = 0;
+}
